40f887832572be2be6b8f8393421f510ef3a529e
Faaiz Sorathiya <114130735+SFaaiz@users.noreply.github.com>
2023-09-08 23:07:28 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -20,3 +20,14 @@ public int longestPalindrome(String s) {
     if(hasSingle) return cnt+1;
     return cnt;
 } 
+
+// 392. Is Subsequence
+public boolean isSubsequence(String s, String t) {
+    if(s.isEmpty()) return true;
+    if(t.isEmpty()) return false;
+    if(s.charAt(0)==t.charAt(0)){
+        return isSubsequence(s.substring(1),t.substring(1));
+    }else{
+        return isSubsequence(s, t.substring(1));
+    }
+}
