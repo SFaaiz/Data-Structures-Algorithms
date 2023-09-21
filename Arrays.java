c7661795dde49ef2c3e612370670f1d46e903ef1
Faaiz Sorathiya <114130735+SFaaiz@users.noreply.github.com>
2023-09-21 09:16:42 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -256,3 +256,17 @@ class Solution {
         return getMedian(merged, merged.length);
     }
 }
+
+// 11. Container With Most Water
+// 
+public int maxArea(int[] height) {
+    int maxArea = 0;
+    int left = 0, right = height.length-1;
+    while(left < right){
+        int curArea = Math.min(height[left],height[right]) * (right-left);
+        maxArea = Math.max(maxArea, curArea);
+        if(height[left]<height[right]) left++;
+        else right--;
+    }
+    return maxArea;
+}
