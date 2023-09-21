5f5ed5fd77693ce4b6ccc414d7fbbc6d83cc1d29
Faaiz Sorathiya <114130735+SFaaiz@users.noreply.github.com>
2023-09-21 09:49:33 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -31,3 +31,19 @@ public boolean isSubsequence(String s, String t) {
         return isSubsequence(s, t.substring(1));
     }
 }
+
+
+// 205. Isomorphic Strings
+public boolean isIsomorphic(String s, String t) {
+    if(s.length() != t.length()) return false;
+    HashMap<Character,Character> match = new HashMap<>();
+    for(int i=0; i<s.length(); i++){
+        char a = s.charAt(i);
+        char b =t.charAt(i);
+        if(!match.containsKey(a) && !match.containsValue(b)){
+            match.put(a,b);
+        }else if(match.containsKey(a) && match.get(a) != b) return false;
+        else if(!match.containsKey(a)) return false;
+    }
+    return true;
+}
