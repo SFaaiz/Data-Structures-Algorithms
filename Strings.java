94b52c53827d9b2377ce655e4e5bc48ae345bf02
Faaiz Sorathiya <114130735+SFaaiz@users.noreply.github.com>
2023-09-22 22:48:33 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -34,6 +34,7 @@ public boolean isSubsequence(String s, String t) {
 
 
 // 205. Isomorphic Strings
+// TC is O(n)
 public boolean isIsomorphic(String s, String t) {
     if(s.length() != t.length()) return false;
     HashMap<Character,Character> match = new HashMap<>();
@@ -46,4 +47,44 @@ public boolean isIsomorphic(String s, String t) {
         else if(!match.containsKey(a)) return false;
     }
     return true;
+}
+
+// 76. Minimum Window Substring
+// sliding window
+
+public String minWindow(String s1, String s2) {
+    int nlen = s1.length(), mlen = s2.length();
+    Map<Character, Integer> toFind = new HashMap<>();
+    Map<Character, Integer> s1map = new HashMap<>();
+    for(char c: s2.toCharArray()){
+        toFind.put(c, toFind.getOrDefault(c, 0) + 1);
+    }
+    char[] arr = s1.toCharArray();
+    int n1=0, n2=0, min = Integer.MAX_VALUE, match=0;
+    int[] res = {0,0};
+    while(n2 < nlen){
+        char c = arr[n2];
+        s1map.put(c, s1map.getOrDefault(c,0)+1);
+        if(s1map.get(c) <= toFind.getOrDefault(c,-1)) match++;
+
+        while(mlen==match){
+            int len = (n2-n1)+1;
+            if(min>len){
+                min = len;
+                res[0]=n1;
+                res[1]=n2;
+            }
+            int cnt = s1map.get(arr[n1]);
+            if(toFind.getOrDefault(arr[n1],-1)>=cnt) match--;
+            s1map.put(arr[n1],s1map.get(arr[n1])-1);
+            if(s1map.get(arr[n1])==0) {
+                s1map.remove(n1);
+            }
+            
+            n1++;
+        }
+        n2++;
+    }
+    if(min == Integer.MAX_VALUE) return "";
+    return s1.substring(res[0],res[1]+1);
 }
